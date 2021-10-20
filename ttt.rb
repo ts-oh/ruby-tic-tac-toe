46c97510f99c1a52d57180b5eb912c31974957d0
ts-oh <tsohlove@gmail.com>
2021-10-20 17:33:17 +0900

    added player switch and input check

--- ttt.rb
+++ ttt.rb
@@ -1,75 +1,143 @@
+require 'pry-byebug'
 require 'io/console'
+
+# Clear command line display to start game
 system('clear')
 
+# Method to display a welcome message with rules
 def welcome_message
-	print "Tic-Tac-Toe Game \n\n\n"
-	print "Rules: \n\n"
-	print "1. Two players, X and O take turns filling in 3x3 grid spaces  \n"
-	print "2. The first player who places 3-marks wins 💪 \n"
-	print "3. Marks must be either horizontal, vertical, or diagonal ← → ↑ ↓ ↖ ↘ \n\n\n"
-	print "Press any key to continue... 😎"
+  print "Tic-Tac-Toe Game \n\n\n"
+  print "Rules: \n\n"
+  print "1. Two players, X and O take turns filling in 3x3 grid spaces  \n"
+  print "2. The first player who places 3-marks wins\n"
+  print "3. Marks must be either horizontal, vertical, or diagonal \n\n\n"
+  print 'Press any key to continue...'
   STDIN.getch
   system('clear')
 end
 
+# Method to display message to start game after name is set
 def press_any_key
-  print 'Press any key to start Tic-Tac-Toe!'
+  print 'If you are ready, press any key to start Tic-Tac-Toe!'
   STDIN.getch
   system('clear')
 end
 
+# Display welcome message method
 welcome_message
 
-# 1. Create a class that creates player objects (name and symbol)
+# Class that creates player objects with name and mark
 class Player
+  # @name and @mark should be readable anywhere
+  attr_reader :name, :mark
+
+  # intialize name and mark for new players
+  # first player mark is always "X" and second player always "O"
   @@player_count = 0
   def initialize
     if @@player_count == 0
       @@player_count += 1
-      @symbol = 'X'
+      @mark = 'X'
       print 'Player 1 Please enter your name: '
-      @name = gets.chomp.capitalize
+      @name = gets.strip.capitalize
     elsif @@player_count == 1
-      @symbol = 'O'
+      @mark = 'O'
       print 'Player 2 Please enter your name: '
-      @name = gets.chomp.capitalize
+      @name = gets.strip.capitalize
 
     end
   end
 
-  def display_players
-    puts "\n Hello #{@name}! You are '#{@symbol}'"
+  # method to greet player and display their name and mark
+  def welcome_players
+    puts "\n Hello #{@name}! You are '#{@mark}'"
   end
 end
 
-new_player1 = Player.new
-puts new_player1.display_players
+# Create player 1 object
+player1 = Player.new
+# Display player 1 name and their mark
+puts player1.welcome_players
 
-new_player2 = Player.new
-puts new_player2.display_players
+# Create player 2 object
+player2 = Player.new
+# Display player 2 name and their mark
+puts player2.welcome_players
 
+# Display message to start game
 press_any_key
 
-# 2. Create a class that creates game board grid (3x3) object
+# Game grid object creating class
 class Board
+  # grid array should be readable and writable
   attr_accessor :grid
-  def create_grid
+
+  # grid is always intialized with 3x3 squares
+  def initialize
     @grid = %w[1 2 3 4 5 6 7 8 9]
+  end
+
+  # method to display the tic-tac-toe grid
+  def display_grid
     puts "\n"
     puts "#{@grid[0]} | #{@grid[1]} | #{@grid[2]}"
-    puts '-----------'
+    puts '---------'
     puts "#{@grid[3]} | #{@grid[4]} | #{@grid[5]}"
-    puts '-----------'
+    puts '---------'
     puts "#{@grid[6]} | #{@grid[7]} | #{@grid[8]}"
     puts "\n"
   end
-	
-  def print_game_grid
-    create_grid
+end
+
+# Create a new grid object to play tic-tac-toe
+game_grid = Board.new
+
+def check_valid_move(valid_move?)
+  if valid_move?(player_position, game_grid) == true
   end
 end
 
-game_board = Board.new
-puts game_board.print_game_grid
+# Method to check for valid move
+def valid_move?(player_position, game_grid)
+  # if gamegrid index is filled with 'X' or 'O'
+  if game_grid.grid[player_position.to_i - 1] == 'X' || game_grid.grid[player_position.to_i - 1] == 'O'
+    false
+  else
+    true
+  end
+end
 
-# 3. Start Game
+# Create a way to have player place their mark on the board
+def turn(game_grid, player1, player2)
+  puts game_grid.display_grid
+  # player counter is zero
+  counter = 0
+
+  # if player1 counter is even (true), then place mark
+  loop do
+    if counter.even?
+      puts "#{player1.name}'s turn!"
+      player_position = gets.strip
+      check_valid_move(valid_move?)
+      game_grid.grid[player_position.to_i - 1] = player.mark
+      counter += 1
+      system('clear')
+      puts game_grid.display_grid
+    # if player 1 is not even (false) then, player 2 turn and place mark
+    else
+      puts "#{player2.name}'s turn!"
+      player_position = gets.strip
+      check_valid_move(valid_move?)
+      game_grid.grid[player_position.to_i - 1] = player.mark
+      counter += 1
+      system('clear')
+      puts game_grid.display_grid
+    end
+  end
+end
+
+turn(game_grid, player1, player2)
+
+
+
+
